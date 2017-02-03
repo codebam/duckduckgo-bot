afda39d497d38a93a53711b6eec817a30404e8b0
Sean Behan <codebam@riseup.net>
2017-02-02 23:58:36 -0500

    fix(main.py): PEP8 fixes
    Literally no code changes, just changes that make it conform more to PEP8

--- main.py
+++ main.py
@@ -15,32 +15,33 @@ Basic inline bot example. Applies different text transformations.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
+import logging
+import configparser
 from uuid import uuid4
-
+from urllib.parse import quote_plus
+from os import environ
+import requests
 from telegram import InlineQueryResultArticle, \
     InputTextMessageContent
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import logging
-from urllib.parse import quote_plus
-import configparser
-from os import environ
-import requests
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
+    """When the bot is issued the command /start"""
     bot.sendMessage(update.message.chat_id, text='''Hello, I am an Inline bot, \
 please use me by mentioning my username in a chat along with your query''')
 
 
 def lmddgtfy_url(query):
+    """Adds your query to the end of the search string and returns a URL"""
     base = "https://lmddgtfy.net/?q="
     end = quote_plus(query)
     url = base + end
@@ -48,12 +49,14 @@ def lmddgtfy_url(query):
 
 
 def shorten_url(long_url):
+    """Puts your URL through the PTPB API to shorten it"""
     response = requests.post('https://ptpb.pw/u', data={'c': long_url})
     url = response.headers.get('Location')
     return url
 
 
 def convert_to_url(query):
+    """Adds your query to the end of the search string and returns a URL"""
     # feel free to fork and change the base url
     base = "https://duckduckgo.com/?q="
     end = quote_plus(query)
@@ -62,6 +65,7 @@ def convert_to_url(query):
 
 
 def inlinequery(bot, update):
+    """Defines the inline query and adds specific functions to it"""
     query = update.inline_query.query
     results = list()
 
@@ -86,11 +90,13 @@ def inlinequery(bot, update):
     bot.answerInlineQuery(update.inline_query.id, results=results)
 
 
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+def ERROR(bot, update, error):
+    """Logs all errors"""
+    LOGGER.warn('Update "%s" caused error "%s"' % (update, error))
 
 
 def change_token():
+    """Change API token with prompt on screen"""
     config = []
     config['config'] = {'token': input('API Key: ')}
     if input('Save? [Y/n]') not in ['n', 'N']:
@@ -101,6 +107,7 @@ def change_token():
 
 
 def main():
+    """Main function to start the bot"""
     try:
         token_ = environ['TELEGRAM_API_KEY']
         # tries to read the api key inside an environment var if it exists
@@ -118,7 +125,7 @@ def main():
             # if there's a keyerror the file probably doesn't exist
             # we fall back to config.ini (the template file)
 
-    if token_ in ['','enter your token here']:
+    if token_ in ['', 'enter your token here']:
         token_ = change_token()
         # if both token files are empty we prompt the user to enter
         # their api key and optionally save it
@@ -127,17 +134,17 @@ def main():
     updater = Updater(token_)
 
     # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(InlineQueryHandler(inlinequery))
+    dispatcher.add_handler(InlineQueryHandler(inlinequery))
 
     # log all errors
-    dp.add_error_handler(error)
+    dispatcher.add_error_handler(ERROR)
 
     # Start the Bot
     updater.start_polling()
